074a222897fc2599a1cfa2c9d468fa2de6980642
jcunha1971 <66164416+jcunha1971@users.noreply.github.com>
2023-05-29 19:28:53 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLA4vnLFaVow25ylLUkb57NhmnN6qk1vE",
+  authDomain: "jair-login.firebaseapp.com",
+  projectId: "jair-login",
+  storageBucket: "jair-login.appspot.com",
+  messagingSenderId: "105428798939",
+  appId: "1:105428798939:web:49a9375f32ea94d4dfebfe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
